5cc8e74eaec17a6e8f0f7397b84e5710db8e61a9
hninthandarkyaw <hninthandarkyaw12344@gmail.com>
2020-09-10 11:11:40 +0630

    edit web.php into default view

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // return view('welcome');
-    return 'Hello World';
+     return view('welcome');
+    //return 'Hello World';
 });
