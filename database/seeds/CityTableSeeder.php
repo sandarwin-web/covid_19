d0c56ddd87497b60c2605aec5582e51ffa22dedd
Winenandar aung <winenandaraung406@gmail.com>
2020-09-12 23:52:18 +0630

    next

--- database/seeds/CityTableSeeder.php
+++ database/seeds/CityTableSeeder.php
@@ -12,6 +12,6 @@ class CityTableSeeder extends Seeder
     public function run()
     {
         //
-        factory(App\City::class,5)->create();
+        factory(App\City::class,6)->create();
     }
 }
